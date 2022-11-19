36e56bb1cc30d1ded169fbebe15d7041b2935709
Korolbm13 <117087714+Korolbm13@users.noreply.github.com>
2022-11-19 13:12:09 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,65 @@
+from flask import Flask, request, json, Response
+from flask_restful import Api, Resource
+from flask_sqlalchemy import SQLAlchemy
+
+
+api = Api()
+db = SQLAlchemy()
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///:memory:"
+db.init_app(app)
+
+
+
+class Course(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    videos = db.Column(db.Integer, nullable=False)
+
+
+with app.app_context():
+    db.create_all()
+
+
+
+class Main(Resource):
+    def get(self, id):
+        course = db.get_or_404(Course, id)
+        return course
+
+    def delete(self, id):
+        #course = db.get_or_404(Course, id)
+        #db.session.delete(course)
+        #db.session.commit()
+        #return
+        pass
+
+
+    def put(self, id):
+       pass
+
+
+class MainList(Resource):
+    def post(self):
+        course = Course(
+            name=request.form["name"],
+            videos=request.form["videos"],
+        )
+        db.session.add(course)
+        db.session.commit()
+       # message = json.dumps({"course": "created"})
+       # resp = Response(message, status=201, mimetype='application/json')
+        return #resp
+
+    def get(self):
+        courses = db.session.execute(db.select(Course).order_by(Course.name)).scalars()
+        return courses
+
+
+api.add_resource(Main, "/api/courses/<int:id>")
+api.add_resource(MainList, "/api/courses/")
+api.init_app(app)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=3000, host="127.0.0.1")
