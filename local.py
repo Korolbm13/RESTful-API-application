378d1e3f4243037f91b79980f43b4c1b3d35db11
Korolbm13 <117087714+Korolbm13@users.noreply.github.com>
2022-12-17 12:16:15 +0700

    Delete local.py

--- local.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-res = requests.post("http://127.0.0.1:3000/api/courses/", {"name": "Golang", "videos": 54})
-res = requests.post("http://127.0.0.1:3000/api/courses/", {"name": "PHP", "videos": 43})
-res = requests.post("http://127.0.0.1:3000/api/courses/", {"name": "C#", "videos": 33})
-res = requests.put("http://127.0.0.1:3000/api/courses/2", {"name": "New", "videos": 32})
-print(res.json())
