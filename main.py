a13c2ba3c196bb6323794a637fcd9382b6a201f7
Korolbm13 <117087714+Korolbm13@users.noreply.github.com>
2022-12-26 16:23:31 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -64,8 +64,9 @@ class MainList(Resource):
         return course
 
     def get(self):
-        page = request.args.get('page', 1, type=int)
-        courses = db.session.query(Course).paginate(page=page, per_page=4)
+        page = request.args.get('page', 2, type=int)
+        per_page = request.args.get('page', 4, type=int)
+        courses = db.session.query(Course).paginate(page=page, per_page=per_page)
         courses = [x.as_dict for x in courses.items]
         return courses
 
